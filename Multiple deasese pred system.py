71eb2d953a5c4d46552580272fc832ee1b08f751
Rathodpradip1 <pradiprathod45@gmail.com>
2025-03-22 17:06:07 +0530

    Update Multiple deasese pred system.py

--- Multiple deasese pred system.py
+++ Multiple deasese pred system.py
@@ -25,8 +25,7 @@ with st.sidebar:
     selected = option_menu('Multiple Disease Prediction System',
                           
                           ['Diabetes Prediction',
-                           'Heart Disease Prediction',
-                           'Parkinsons Prediction'],
+                           'Heart Disease Prediction'],
                           icons=['activity','heart','person'],
                           default_index=0)
     
